763d0bac45202133f83bd96965e70f26ad41e38f
Jared Strohschein <45718292+jstrohschein@users.noreply.github.com>
2020-07-26 00:17:35 -0400

    Mostly funtional, just working on stretch

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -10,18 +10,18 @@ const Post = props => {
   return (
     <div className='post-border'>
       <PostHeader
-        username={post.username}
-        thumbnailUrl={post.thumbnailUrl}
+        username={props.postData.username}
+        thumbnailUrl={props.postData.thumbnailUrl}
       />
       <div className='post-image-wrapper'>
         <img
           alt='post thumbnail'
           className='post-image'
-          src={post.imageUrl}
+          src={props.postData.imageUrl}
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      <LikeSection likePost={() => likePost(post.id)} numOfLikes={props.postData.likes} />
       {/* Comments also wants its props! */}
       <Comments />
     </div>
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -6,10 +6,20 @@ const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
 
+  
+
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+
+      {/*posts = props
+         dataFromApp = dummyData (an array of objects) */}
+
+      {props.dummyData.map((post, index) => {
+        return <Post key={index} postData={post} />
+      })}      
       {/* Check the implementation of Post to see what props it requires! */}
+
     </div>
   );
 };
